896df4ec84e6832b318592700de5c9569e8e21c1
NewUser <nollyenterprise@gmail.com>
2023-04-26 07:29:45 +0100

    Commit 2023-04-26 07:00:00

---
+++ day1.js
@@ -0,0 +1,13 @@
+// console.log(document)
+// ARRAY
+// const colorVariations = [ "blue", "green", "black", "orange", "violet" ];
+
+// // define an Interval
+// setInterval(() => {
+//     let cont1 = document.getElementsByClassName("change-color");
+//     let cont2 = document.querySelector(".change-font");
+//     cont1.style.backgroundColor = colorVariations[Math.floor(Math.random()*colorVariations.length)];
+//     cont2.style.color = colorVariations[Math.floor(Math.random()*colorVariations.length)];
+
+// // Run every 3 seconds 
+// }, 3000);
---
+++ day3.js
@@ -0,0 +1,43 @@
+// HOISTING ================
+
+// Let Const and Var 
+// Global Scoped Variables =======
+const user_data = {
+    name: "Paul Foh",
+    country: "Nigeria",
+    address:  ["7 Badamosi street", "Shomolu", "Lagos"],
+    age: 45,
+    dob: "2000-05-01",
+    current_time: new Date(),
+    test_empty: ""
+};
+console.log(user_data);
+
+// var name;
+// console.log(name);
+// name='chibuzor';
+
+function get_user_name (data1, data2, data3 = "age") {
+    // Blocked Scoped Variable 
+    let uname = user_data[data1] + ' ' + user_data[data2] + ' ' + user_data[data3];
+
+    return uname;
+}
+
+// const username = get_user_name ('name', 'country', 'dob');
+// console.log(username);
+
+function check_if_user_data_is_empty (udata) {
+    console.log(udata);
+    let err = {};
+    for (const e in udata) {
+        console.log(e)
+        if(udata[e] == ""){
+            err[e] = e+" is empty.";
+        }
+    }
+    return err;
+}
+
+let response = check_if_user_data_is_empty (user_data);
+console.log(response);
